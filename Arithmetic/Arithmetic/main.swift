81418d0ac4329fb9183ed83364620d0984431afa
qingquan <qingquan.zhang@niutron.com>
2023-04-26 17:36:39 +0800

    initiate

---
+++ Arithmetic/Arithmetic/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Arithmetic
+//
+//  Created by qingquan on 2022/12/27.
+//
+
+import Foundation
+
+
+//let result = searchRange([2, 2], 2)
+
+let result = minSpeedOnTime([1, 3, 2], 2.7)
+print(result)
+
+let r = [1, 2, 2].enumerated().filter { objc in
+    let i = objc.offset
+}
+
